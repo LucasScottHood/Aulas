dc9f12f7bdf14bc18fffc8f7a9abe80de6321bd9
LucasScottHood <2024315321@aluno.riogrande.ifrs.edu.br>
2025-11-12 00:36:57 -0300

    Enhance task management with completed tasks feature
    Added functionality to track completed tasks and display them separately.

--- TrabalhoToDo/js/código.js
+++ TrabalhoToDo/js/código.js
@@ -4,13 +4,14 @@ document.querySelector('#recuperar').addEventListener('click', recuperar);
 document.querySelector('#limpar').addEventListener('click', limpar);
 
 let toDoList = [];
+let tarefasFeitas = [];
 
 function mostra() {
   let html = "";
   for (let i = 0; i < toDoList.length; i++) {
     html += `
       <div class="tarefa-item">
-        <input type="checkbox" id="tarefa${i}" onclick="${toDoList[i].feito = true}">
+        <input type="checkbox" id="tarefa${i}" onclick="marcarTarefa(${i})">
         <label for="tarefa${i}">
         ${toDoList[i].texto}<span class="data">(${toDoList[i].data}) - Prioridade ${toDoList[i].prioridade}</span>
         <input type="button" value="Excluir" id="tarefa${i}" onclick="excluirTarefa(${i})">
@@ -29,15 +30,45 @@ function insereTarefa() {
   const [ano, mes, dia] = data.split("-");
   data = `${dia}/${mes}/${ano}`
   const prioridade = String(document.querySelector("#prioridade").value);
-  const feito = "checked";
     toDoList.push({
     texto: texto,
     data: data,
     prioridade: prioridade,
-    feito: feito,
 });
   input.value = "";
   mostra();
+}
+
+function marcarTarefa(index) {
+  const tarefaFeita = toDoList.splice(index, 1);
+  tarefasFeitas.push(tarefaFeita);
+  mostra();
+  mostraFeitas();
+}
+
+function mostraFeitas() {
+  let html = "<h2>Trabalhos Feitos:</h2>";
+  for (let i = 0; i < tarefasFeitas.length; i++) {
+    html += `
+      <div class="tarefa-item feita">
+        <input type="checkbox" checked disabled>
+        <label>
+          ${tarefasFeitas[i].texto}
+          <span class="data">
+            (${tarefasFeitas[i].data}) - Prioridade ${tarefasFeitas[i].prioridade}
+          </span>
+        </label>
+        <input type="button" value="Excluir" onclick="excluirFeita(${i})">
+      </div>`;
+  }
+  let listaFeitas = document.querySelector("#listaFeita");
+  if (!listaFeitas) 
+    {
+    listaFeitas = document.createElement("div");
+    listaFeitas.id = "listaFeita";
+    document.querySelector("main").appendChild(listaFeitas);
+  }
+  listaFeitas.innerHTML = html;
 }
 
 function excluirTarefa(index) {
@@ -45,22 +76,27 @@ function excluirTarefa(index) {
   mostra();
 }
 
+function excluirFeita(index) {
+  tarefasFeitas.splice(index, 1);
+  mostraFeitas();
+}
+
 function gravar() {
   localStorage.setItem("lista", JSON.stringify(toDoList));
+  localStorage.setItem("feitas", JSON.stringify(tarefasFeitas));
 }
 
 function recuperar() {
-    toDoList=[];
-    lista=localStorage.getItem("lista");
-    if(lista){
-      toDoList=JSON.parse(lista);
-    }
-    mostra();
+  toDoList = JSON.parse(localStorage.getItem("lista")) || [];
+  tarefasFeitas = JSON.parse(localStorage.getItem("feitas")) || [];
+  mostra();
+  mostraFeitas();
 }
 
 function limpar() {
   localStorage.removeItem("lista");
   toDoList = [];
+  tarefasFeitas = [];
   mostra();
 }
 
